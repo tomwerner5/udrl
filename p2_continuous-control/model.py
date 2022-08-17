2b2d8fe2abaa83840e9b2a467ab1d3ae60715a26
Tom <tomwerner5@gmail.com>
2022-08-17 01:11:14 -0600

    Updated documentation

--- p2_continuous-control/model.py
+++ p2_continuous-control/model.py
@@ -2,91 +2,6 @@ import torch
 import torch.nn as nn
 
 
-# class Actor(nn.Module):
-#     """Actor (Policy) Model."""
-#
-#     def __init__(self, state_size, action_size, seed):
-#         """Initialize parameters and build model.
-#
-#         Params
-#         ======
-#             state_size (int): Dimension of each state
-#             action_size (int): Dimension of each action
-#             seed (int): Random seed
-#         """
-#         super(Actor, self).__init__()
-#         self.seed = torch.manual_seed(seed)
-#         self.state_size = state_size
-#         self.action_size = action_size
-#
-#         self.h1 = nn.Linear(state_size, 64)
-#         self.h2 = nn.Linear(64, 64)
-#         self.h3 = nn.Linear(64, action_size)
-#
-#     def forward(self, state):
-#         """Network that maps state -> action values.
-#
-#         Params
-#         ======
-#             state (array): state values to pass to the network
-#         """
-#         x = torch.relu(self.h1(state))
-#         x = torch.relu(self.h2(x))
-#         x = torch.tanh(self.h3(x))
-#         return x
-#
-#
-# class Critic(nn.Module):
-#     """Actor (Policy) Model."""
-#
-#     def __init__(self, state_size, action_size, seed):
-#         """Initialize parameters and build model.
-#         Params
-#         ======
-#             state_size (int): Dimension of each state
-#             action_size (int): Dimension of each action
-#             seed (int): Random seed
-#         """
-#         super(Critic, self).__init__()
-#         self.seed = torch.manual_seed(seed)
-#         self.state_size = state_size
-#         self.action_size = action_size
-#
-#         # input/hidden layer
-#         self.h1_a = nn.Linear(action_size, 64)
-#         self.h1_s = nn.Linear(state_size, 32)
-#
-#         # concat layer
-#         self.h2_c = nn.Linear(64+32, 64)
-#
-#         # output layer
-#         self.h2_out = nn.Linear(64, 1)
-#
-#     def forward(self, state, action):
-#         """Network that maps state -> action values.
-#
-#         Params
-#         ======
-#             state (array): states to pass to the network
-#             action (array): actions to pass to the network
-#         """
-#         xs = torch.relu(self.h1_s(state))
-#         xa = torch.relu(self.h1_a(action))
-#         x = torch.relu(self.h2_c(torch.cat((xs, xa), dim=1)))
-#         value = self.h2_out(x)
-#
-#         return value
-
-
-
-############### This got there #################
-# BUFFER_SIZE = int(1e5)  # replay buffer size
-# BATCH_SIZE = 128  # minibatch size
-# GAMMA = 0.99  # discount factor
-# TAU = 1e-3  # for soft update of target parameters
-# LR_ACTOR = 1e-4  # learning rate
-# LR_CRITIC = 1e-4  # learning rate
-# UPDATE_EVERY = 1  # how often to update the network
 class Actor(nn.Module):
     """Actor (Policy) Model."""
 
@@ -120,10 +35,6 @@ class Actor(nn.Module):
         x = torch.tanh(self.h3(x))
         return x
 
-    def _print(self):
-        print('_'.join([str(p.shape) for p in self.parameters()]))
-
-
 
 class Critic(nn.Module):
     """Actor (Policy) Model."""
@@ -141,8 +52,10 @@ class Critic(nn.Module):
         self.state_size = state_size
         self.action_size = action_size
 
-        # input/hidden layer
+        # input/hidden layer for action space
         self.h1_a = nn.Linear(action_size, 32)
+
+        # input/hidden layers for state space
         self.h1_s_1 = nn.Linear(state_size, 64)
         self.h1_s_2 = nn.Linear(64, 128)
 
@@ -169,6 +82,3 @@ class Critic(nn.Module):
         value = self.h2_out_2(x)
 
         return value
-
-    def _print(self):
-        print('_'.join([str(p.shape) for p in self.parameters()]))
--- p2_continuous-control/td3_agent.py
+++ p2_continuous-control/td3_agent.py
@@ -15,9 +15,7 @@ LR_CRITIC_1 = 1e-3  # learning rate
 LR_CRITIC_2 = 1e-3  # learning rate
 UPDATE_EVERY = 1  # how often the agent should learn
 POLICY_UPDATE_FREQUENCY = 2 # How often to update the policy
-CRITIC_MAX_GRAD_NORM = 1  # gradient clipping max for critic
-ACTOR_MAX_GRAD_NORM = 1  # gradient clipping max for actor
-SEED = 1
+SEED = 1  # random state
 
 np.random.seed(SEED)
 random.seed(SEED)
@@ -34,27 +32,30 @@ class Agent():
             state_size (int): dimension of each state
             action_size (int): dimension of each action
             seed (int): random seed
-            actor (object): actor network in DDPG, a pytorch object of type torch.nn.Module or similar
-            critic (object): critic network in DDPG, a pytorch object of type torch.nn.Module or similar
+            actor (object): actor network in TD3, a pytorch object of type torch.nn.Module or similar
+            critic (object): critic network in TD3, a pytorch object of type torch.nn.Module or similar
             device (str): device for value placement during training, either gpu or cpu (e.g. `cuda:0`)
         """
         self.state_size = state_size
         self.action_size = action_size
         self.device = device
 
-        # Q-Network
+        # TD3 local and target networks creation
         self.actor_local = actor(state_size, action_size, SEED).to(device)
         self.actor_target = actor(state_size, action_size, SEED).to(device)
         self.critic_1_local = critic(state_size, action_size, SEED).to(device)
         self.critic_1_target = critic(state_size, action_size, SEED).to(device)
         self.critic_2_local = critic(state_size, action_size, SEED+1).to(device)
         self.critic_2_target = critic(state_size, action_size, SEED+1).to(device)
+
+        # network optimizers
         self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
         self.critic_1_optimizer = optim.Adam(self.critic_1_local.parameters(), lr=LR_CRITIC_1)
         self.critic_2_optimizer = optim.Adam(self.critic_2_local.parameters(), lr=LR_CRITIC_2)
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, device)
+
         # Initialize time step (for updating every UPDATE_EVERY steps)
         self.t_step = 0
 
@@ -63,6 +64,17 @@ class Agent():
         self.action_noise = GaussianNoise(action_size, sigma=0.1)
 
     def step(self, state, action, reward, next_state, done, t):
+        """Take a step in the environment.
+
+        Params
+        ======
+            state (array-like): current state
+            action (array-like): current action
+            reward (float): current reward
+            next_state (array-like): immediate next state
+            done (bool): boolean for terminal state
+            t (int): current time step
+        """
         # Save experience in replay memory
         self.memory.add(state, action, reward, next_state, done)
 
@@ -85,10 +97,14 @@ class Agent():
         state = torch.from_numpy(state).float().to(self.device)
         self.actor_local.eval()
         with torch.no_grad():
+            # calculate actions using policy network
             actions = self.actor_local(state)
         self.actor_local.train()
         if add_noise:
+            # add noise to the action selection
             actions = actions + self.action_noise(False)
+
+        # clip the action to the action space min/max
         return actions.clamp(-1, 1)
 
     def learn(self, experiences, gamma, t):
@@ -98,16 +114,24 @@ class Agent():
         ======
             experiences (Tuple[torch.Variable]): tuple of (s, a, r, s', done) tuples
             gamma (float): discount factor
+            t (int): current time step
         """
+        # sampled experiences from buffer
         states, actions, rewards, next_states, dones = experiences
 
+        # Calculate next actions using the clipped noise
         next_actions = self.actor_target(next_states) + self.policy_noise(True)
+
+        # calculate action-values from each critic network using the calculated next actions
         q_target_1 = self.critic_1_target(next_states,
                                           next_actions)
         q_target_2 = self.critic_2_target(next_states,
                                           next_actions)
+
+        # calculate the overall action value of the targets by using the minimum of the two critic values
         q_target_value = rewards + (gamma * torch.min(q_target_1, q_target_2) * (1 - dones))
 
+        # calculate the action value of the local networks using sampled values
         q_local_value_1 = self.critic_1_local(states,
                                               actions)
         q_local_value_2 = self.critic_2_local(states,
@@ -117,18 +141,12 @@ class Agent():
         critic_1_loss = F.mse_loss(q_target_value, q_local_value_1)
         self.critic_1_optimizer.zero_grad()
         critic_1_loss.backward(retain_graph=True)
-        # torch.nn.utils.clip_grad_norm_(
-        #     self.critic_1_local.parameters(),
-        #     CRITIC_MAX_GRAD_NORM)
         self.critic_1_optimizer.step()
 
         # critic 2 update
         critic_2_loss = F.mse_loss(q_target_value, q_local_value_2)
         self.critic_2_optimizer.zero_grad()
         critic_2_loss.backward()
-        # torch.nn.utils.clip_grad_norm_(
-        #     self.critic_2_local.parameters(),
-        #     CRITIC_MAX_GRAD_NORM)
         self.critic_2_optimizer.step()
 
         # actor update
@@ -137,9 +155,6 @@ class Agent():
             actor_loss = -self.critic_1_local(states, actions_sample).mean()
             self.actor_optimizer.zero_grad()
             actor_loss.backward()
-            # torch.nn.utils.clip_grad_norm_(
-            #     self.actor_local.parameters(),
-            #     ACTOR_MAX_GRAD_NORM)
             self.actor_optimizer.step()
 
             # ------------------- update target network ------------------- #
@@ -161,11 +176,13 @@ class Agent():
             target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
     def save(self):
+        """Save the model weights"""
         torch.save(self.actor_local.state_dict(), 'actor_checkpoint.pth')
         torch.save(self.critic_1_local.state_dict(), 'critic_1_checkpoint.pth')
         torch.save(self.critic_2_local.state_dict(), 'critic_2_checkpoint.pth')
     
     def _print(self):
+        """Print the hyperparameters used to train the agent"""
         msg = ("HYPERPARAMETERS:\n---------------\n\n"
               "BUFFER_SIZE = {0}\n"
               "BATCH_SIZE = {1}\n"
@@ -183,9 +200,6 @@ class Agent():
                                             LR_CRITIC_2,
                                             UPDATE_EVERY))
         print(msg)
-
-        self.actor_local._print()
-        self.critic_local._print()
         
 
 class ReplayBuffer:
